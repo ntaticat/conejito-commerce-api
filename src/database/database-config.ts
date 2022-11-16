cc54ef5162e6eda28b08460f0a6332562a5abf65
Rafael Estrada <ntaticat@gmail.com>
2022-11-16 12:11:48 -0600

    unrecognize changes

--- src/database/database-config.ts
+++ src/database/database-config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    const connectionString = process.env.NODE_ENV === "test" ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI;
+    const connectionString = process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development" ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI;
     const connection = await mongoose.connect(connectionString || "YOUR CONNECTION STRING");
     console.log("base de datos conectada");
     return connection;
